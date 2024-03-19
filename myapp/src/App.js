805007a33320340ecb1c3a52fc960513d6c336a7
DebCh20 <1804020@kiit.ac.in>
2024-03-19 08:58:04 +0530

    React tutorial commits

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Increment from './Components/Increment.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello all</h1>
+      <Increment/>
+    </div>
+  );
+}
+
+export default App;
